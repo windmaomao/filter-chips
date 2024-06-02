681bc3de7053e893e0e074c6645c818e1dc519ab
Fang Jin <windmaomao@gmail.com>
2024-06-02 09:46:19 -0400

    init repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import design from './design.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <h1>Filters Demo</h1>
+      <img
+        className={styles.pic}
+        src={design}
+        alt="Design"
+        width="100%"
+      />
+      <div>blahblahbalh</div>
     </div>
   );
 }
